86bc2f86cb8cfa58e1d8f7d0a8689d65fcada283
Pratham Gupta <prathagu@cisco.com>
2024-08-10 10:43:23 +0530

    Enhance terminal

--- terminal/app.js
+++ terminal/app.js
@@ -1,6 +1,7 @@
 // CONSTANTS
-
-
+const APP_VERSION_STRING = "1.1.18";
+const CODE_INPUT_FIELD_ID = 'codeInputField';
+const GET_STARTED_NODE_ID = 'getStartedNodeId';
 
 
 // FUNCTIONS
@@ -15,10 +16,160 @@ function setBackgroundColor() {
     document.body.style.background = `linear-gradient(${angle}deg, ${colorA}, ${colorB})`;
 }
 
+/**
+ * Get the last child node of a parent node
+ * @param {HTMLElement} parentNode
+ * @returns {Node} last child node
+ */
+function getLastTextChildNode(parentNode) {
+    return parentNode.childNodes[parentNode.childNodes.length - 1];
+}
+
+/**
+ * Set the app version in the terminal header
+ * @param {string} app_version_string
+ * @returns {void}
+ * @description Set the app version in the terminal header
+*/
+function setAppVersion(app_version_string) {
+    var terminalHeadVersionChild = getLastTextChildNode(document.getElementById("terminalHead"));
+    var terminalHeaderString = " cat ./about.txt(v" + app_version_string + ")";
+    terminalHeadVersionChild.textContent = terminalHeaderString;
+}
+
+/**
+ * Get the node content which can be typed
+ * @param {string} nodeId
+ * @returns {HTMLElement} node content
+ */
+function printResultCharByChar(node, charPrintWaitTime = 140, cmd) {
+
+    cursor.className = 'cursor blink';
+    node.parentNode.appendChild(cursor);
+
+    let itrIndex = 0;
+    const i = setInterval(() => {
+
+        if (!node || itrIndex == (node._saved.length + 1)) {
+            clearInterval(i);
+            return;
+        }
+
+        node.textContent = node._saved.slice(0, itrIndex);
+
+        if (itrIndex == node._saved.length) {
+            cursor.className = 'cursor blink';
+            appendInputStrip(node, 1000, cmd);
+
+        } else {
+            node.parentNode.appendChild(cursor);
+            cursor.className = 'cursor';
+        }
+        itrIndex++;
+
+    }, charPrintWaitTime);
+
+    // console.log("Debug: ending printResultCharByChar");
+}
+
+function printResultAndAppendinputCommandStrip(node, printWaitTime, cmd = '') {
+    printResultCharByChar(node, printWaitTime, cmd);
+}
+
+/**
+ * Fetches the iteratable content from the given NodeId
+ * @params takes in the nodeID
+ * @returns returns a list of a node which is to be iterated.
+ */
+function getTypeableNodeContent(nodeId) {
+    const p = document.getElementById(nodeId);
+    const node = p.childNodes[p.childNodes.length - 1];
+    // replace multiple spaces with a single space
+    node._saved = cleanupData(node.textContent);
+    node.textContent = '';
+    return node;
+}
+
+/**
+ * Cleans up the data by removing leading and trailing spaces
+ * @param {string} data
+ * @returns {string} cleaned up data
+ */
+function cleanupData(data) {
+    return data.trim();
+}
+
+function appendInputStrip(node, delay, cmd) {
+    setTimeout(() => {
+        cursor.className = 'cursor blink';
+        inputBlock.appendChild(cursor)
+        node.parentNode.appendChild(inputCommandStrip);
+        inputCommandStrip.style.display = "block";
+        if (cmd.includes('random')) {
+            inputBlock.value = 'random';
+        }
+        else {
+            inputBlock.value = 'help';
+        }
+        // highlightCodeInputField();
+    }, delay);
+}
+
+// // Initial Setup (Initialization)
+// Setup cursor
+const cursor = document.createElement('span');
+cursor.innerHTML = '_';
+cursor.className = 'cursor blink';
+
+let inputCommandStrip = document.getElementById('inputCommandStrip')
+let inputBlock = document.getElementById(codeInputField);
 
 
 
 
-// Main Call starts here:
+// // Main Call starts here:
 setBackgroundColor();
+setAppVersion(APP_VERSION_STRING);
 
+const getStartedNode = getTypeableNodeContent(GET_STARTED_NODE_ID);
+setTimeout(function () {
+    printResultAndAppendinputCommandStrip(getStartedNode, 50);
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// input Command Strip, set the keyup event for ENTER key
+// inputBlock.addEventListener("keyup", function (event) {
+//     event.preventDefault();
+//     if (event.keyCode === 13) {
+//         executeCommand(inputBlock.value);
+//         // document.getElementById("id_of_button").click();
+//     }
+// });
+
+
+// /**
+//  * Highlights the inputCommandStrip area and moves the focus of the pointer to it.
+//  */
+// function highlightCodeInputField() {
+//     document.getElementById(CODE_INPUT_FIELD_ID).focus();
+//     document.getElementById(CODE_INPUT_FIELD_ID).select();
+// }
