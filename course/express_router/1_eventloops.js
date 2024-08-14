724cd2681a447c559d7826b5e6a5e2cba8437d08
Pratham Gupta <prathagu@cisco.com>
2024-08-14 22:38:37 +0530

    add express router code

--- course/express_router/1_eventloops.js
+++ course/express_router/1_eventloops.js
@@ -1,15 +1,15 @@
 console.log('Start');
 
-setTimeout(() => {
-    console.log('setTimeout');
-}, 0);
-
 Promise.resolve()
     .then(() => {
         console.log('Promise');
     });
 
-console.log('End');
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+
+// console.log('End');
 
 
 
