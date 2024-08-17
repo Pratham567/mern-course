cd07c45014c31de8852f0abee14ba8e93eb33ed9
Pratham Gupta <prathagu@cisco.com>
2024-08-17 08:02:48 +0530

    Terminal wrap up

--- terminal/app.js
+++ terminal/app.js
@@ -9,18 +9,231 @@ const RESUME_URL = "https://bit.ly/ResumePratham";
 const LINKEDIN_URL = "https://linkedin.com/in/pratham567";
 const GITHUB_URL = "https://github.com/Pratham567";
 
+// Command related constants
+const ALL_SUPPORTED_CMDS = "help, resume, bio, linkedin, random, github, contact, date. But don't try to gain access by running commands like sudo";
+const CMDS_COMING_SOON = "These commands will come soon: projects, sama, clear, new, man, home"
+
+
 // club commands based on results
-const specialCmds = ['sama', 'projects'];
-const debugCmds = ['h'];
+const SPECIAL_CMDS = ['sama', 'projects'];
+const DEBUG_CMDS = ['h'];
 const RESUME_CMDS = ['resume', 'biodata', 'cv'];
-const bioCmds = ['bio', 'about', 'biography', 'info', 'intro'];
-const contactCmds = ['contact', 'email'];
+const BIO_CMDS = ['bio', 'about', 'biography', 'info', 'intro'];
+const CONTACT_CMDS = ['contact', 'email'];
 const LINKEDIN_CMDS = ['linkedin'];
 const GITHUB_CMDS = ['github', 'git', 'gitlab', 'vcs'];
-const randomCmds = ['random'];
-const blockedCmds = ['sudo', 'root', 'su', 'admin', 'superuser', 'shutdown', 'reboot', 'restart', 
-                     'patch', 'update', 'daemon', 'systemctl', 'edit', 'modify'];
+const RANDOM_CMDS = ['random'];
+const BLOCKED_CMDS = ['sudo', 'root', 'su', 'admin', 'superuser', 'shutdown', 'reboot', 'restart',
+    'patch', 'update', 'daemon', 'systemctl', 'edit', 'modify'];
 const dateCmds = ['date', 'time'];
+
+// Prefixes
+const FAILED_RESULT_PREFIX_ACTION_WORDS_LIST = ['Oops', 'Ahh', 'Oh no', "I'm sorry",
+    'Apologies', 'Whoops', 'Uh-oh',
+    "That's bad", 'Yikes', 'Oh boy',
+    'Dang', 'Pardon', 'Shoot',
+    "Geez", "My bad", "Bummer", "Oh dear",
+    "Drat", "Alas", "Aw man", "Rats",
+    "Well, that's not good", "Oh gosh",
+    "Sorry about that", "Fiddlesticks",
+    "Oopsie daisy", "Darn it", "Ouch",
+    "Mea culpa", "How unfortunate"
+];
+
+const FAILED_RESULT_PREFIX = ["this command is not possible",
+    "this is invalid",
+    "this seems incorrect",
+    "that seems invalid",
+    "this is an invalid request",
+    "this is incorrect",
+    "this cannot be done",
+    "this command cannot be executed",
+    "that's not a valid option",
+    "that's an invalid request",
+    "that's not allowed",
+    "this action is not permitted",
+    "this request is not recognized",
+    "that's not a valid input",
+    "I can't do that",
+    "the command is not available yet",
+    "the system is unable to process that request",
+    "the request is not supported",
+    "the request is not possible at this time",
+    "that's not a valid operation",
+    "I'm afraid I can't comply with that request",
+    "that's an unrecognized command",
+    "I cannot recognize that command",
+    "I cannot recognize the request",
+    "I cannot fulfill that command",
+    "I cannot fulfil that request",
+    "that's not a valid choice",
+    "that's not a valid selection",
+    "that's not a supported option",
+    "that's not a valid command",
+    "I don't understand the request",
+    "I don't understand the command",
+    "the input is not recognized",
+    "the input is not valid",
+    "the input is incoorect",
+    "the input cannot be processed",
+    "the request is not feasible",
+    "the request is not allowed",
+    "the request is not permitted",
+    "that's not a valid request",
+];
+
+const GREET_KEYWORDS = ['Hello (English)', 'Bonjour (French)', 'Guten Tag (German)', 'Ciao (Italian)',
+    'Hola (Spanish)', 'Salut (Romanian)', 'Hallo (Dutch)', 'Hej (Swedish)', 'Olá (Portuguese)',
+    'Ni hao (Mandarin Chinese)', 'Konnichiwa (Japanese)', 'Annyeonghaseyo (Korean)',
+    'Salaam (Arabic)', 'Shalom (Hebrew)', 'Namaste (Hindi)', 'Sawubona (Zulu)',
+    'Jambo (Swahili)', 'Ayubowan (Sinhala)', 'Marhaba (Moroccan Arabic)', 'Saluton (Esperanto)'
+];
+
+const HELP_CMD_PREFIX_LIST = ["Try one of the following commands",
+    "Only the following commands are permitted",
+    "You can only use either of the following commands",
+    "You can only use one of the following commands",
+    "Please use either of the following commands",
+    "Only the following requests are allowed",
+    "Only the following commands are allowed",
+    "Request will only be processed for the following commands",
+    "Request will only be processed for the following options",
+    "Use either of the following options",
+    "I can only responsed to the following commands",
+    "I can only answer to the following commands",
+    "Please choose one of the following commands",
+    "You are only permitted to use one of the following commands",
+    "You can use the following commands",
+    "Only the following requests will be processed",
+    "These are the only commands that are allowed",
+    "You may only choose from the following options",
+    "The following commands are the only ones that will be recognized",
+    "The system will only respond to the following commands",
+    "These are the only valid commands",
+    "The following commands are the only ones that will work",
+    "The following commands are supported",
+    "The following options are available",
+    "Please choose from the following authorized commands",
+    "These are the only acceptable commands",
+    "Please use one of the following valid options",
+    "The system will only accept the following requests",
+    "These are the only commands that will be recognized by the system",
+    "Only the following inputs will be processed by the system",
+    "The following commands are the only ones that will be executed",
+    "The following commands are the only ones that are currently available",
+    "The system can only respond to the following authorized commands",
+    "Only the following actions are allowed",
+    "The following actions are the only ones that will be allowed",
+    "These are the only permissible commands for this operation",
+    "These are the only commands that the system is configured to handle",
+    "Please use one of the following supported commands",
+    "The following actions are the only ones that will be processed",
+    "The system recognizes only the following commands",
+    "The system will only respond to the following authorized inputs",
+    "Only the following operations are allowed in this context",
+    "Please use one of the following available commands",
+    "The following commands are the only ones that will result in successful processing",
+    "These are the only commands that are currently supported",
+    "Please choose from the following list of available commands",
+    "Only the following inputs will result in successful processing",
+    "These are the only commands that will be processed by the system",
+    "Only the following requests will be processed",
+    "The following commands are the only ones that will work",
+    "These are the only commands that will be processed successfully by the system",
+];
+
+const BIO_RESULT = "Pratham is a software developer currently working at Cisco 5G team. He builds highly scalable distributed network applications using some of the best industry practices when it comes to managing and monitoring those applications. \
+He has experience in building common libraries so that the developers can focus more on business logic, avoid code duplication and develop faster. \
+He has also built highly distributed pipelines for efficient testing and deployments. \
+He is fascinated by Cloud and Data. Cyber Security takes up most of his free time. If you are into security, you'll vibe :p";
+
+const RESUME_RESULT = "Thanks for the query. Get my resume here: " + RESUME_URL + ". Hold on, opening in a new tab. Please check if the pop-ups are not blocked";
+
+const CONTACT_RESULT = "Pratham is reachable at: go4pratham0897@gmail.com. You maybe be looking for the following commands: linkedin, github, about";
+
+const LINKEDIN_RESULT = "Connect with Pratham on LinkedIn here: " + LINKEDIN_URL + ". Hold on, opening in a new tab. Please check if the pop-ups are not blocked";
+
+const GITHUB_RESULT = "Most of the Pratham's contribution goes to github enterprise, hence they may not be visible in his public profile. Here is Pratham's github url: " + GITHUB_URL + ". Hold on, opening in a new tab. Please check if the pop-ups are not blocked";
+
+const BLOCKED_CMD_RESULT_LIST = ["I cannot let you run that",
+    "There's no way I can let you run that",
+    "Nice try, but I can't let that happen",
+    "That's not something I can allow",
+    "I'm sorry, but I can't let you do that",
+    "I'm afraid I can't authorize that action",
+    "I can't give you permission to do that",
+    "Unfortunately, that operation is not permitted",
+    "Access denied. That action cannot be executed",
+    "That request cannot be authorized at this time",
+    "Sorry, that action is restricted",
+    "I'm sorry, but I cannot grant you the necessary access to do that",
+    "That operation is outside the scope of your authority",
+    "I'm sorry, but that action goes beyond the limits of what I can permit",
+    "I'm sorry, but that action is not permitted for you",
+    "I'm afraid I cannot comply with that request",
+    "I'm sorry, but that request cannot be processed",
+    "I'm sorry, but I don't have the authority to permit that",
+    "I'm afraid I cannot execute that",
+    "That request is not possible given the current system configuration",
+    "I'm sorry, but that feature is not permitted at this time",
+    "Unfortunately, that action is not allowed by this system",
+    "I'm afraid I cannot grant you the necessary permissions to do that",
+    "That request goes beyond the scope of current user",
+    "I'm sorry, but that action is prohibited",
+    "Access to that function is restricted for security reasons",
+    "I'm afraid that action is beyond the scope of current user",
+    "I'm sorry, but that action cannot be performed for technical reasons",
+    "I'm afraid I cannot comply with that request due to system restrictions",
+    "I'm sorry, but that request cannot be fulfilled due to system constraints"
+];
+
+const RANDOM_RESULT_LIST = ["Pratham has a bachelors degree in Chemical Engineering.",
+    "Pratham has won the President's Gold Medal Award for his all round performance and his contributions to the department/college/society..",
+    "Pratham has travelled to 3 different countries.",
+    "Pratham has been very active in sports and have won multiple medls during college and holds records like maximum push-ups, maximum pull-ups in a go.",
+    "Pratham has learnt development by himself.",
+    "Pratham has a deep interest in cybersecurity, specially in reverse engineering.",
+    "Pratham files taxes by himself.",
+    "Pratham has won medals in body-building competitions.",
+    "Pratham like to eat neew things, but he's vegetarian.",
+    "Pratham's favourite fruit is apple.",
+    "Please try again....",
+    "Pratham believes in giving back to the society, hence he has been part of various social initiatives in college.",
+    "Pratham has volunteered with Robinhood Army.",
+    "Pratham has published a research paper on biofuel production from wheat straw in pre-final year of his bachelor's.",
+    "Pratham has been a student mentor in his college.",
+    "Pratham, along with a few other Alumni of IITR, has developed Alumni Mentorship Program for the benefit of current students of IIT.",
+    "Pratham loves to travel.",
+    "Pratham dreams of having a Vanity Van one day.",
+    "Pratham dreams of setting up a Research and Development Lab in India.",
+    "Pratham wants to stay in India in long term.",
+    "For Pratham, job quality matters more than money.",
+    "Pratham has refused offers from big companies like Amazon, Oracle....",
+    "Pratham loves to reach technical blogs, not he doesn't want to go for higher studies.",
+    "Prtham's friend call hin: SAMA.",
+    "Pratham likes to play badminton.",
+    "Pratham was part of team Athletics of IIT Roorkee.",
+    "Pratham has run marathon since he was in first year at college.",
+    "Pratham can write code in multiple languages.",
+    "This website was developed by Pratham, just for fun.",
+    "Pratham believes more in long term investment than short term swings.",
+    "Pratham can talk about sports, finance, life, games, cars.",
+    "Pratham is reachable at: go4pratham0897@gmail.com",
+    "This website doesn't use AI, but may use in future.",
+    "Pratham was awarded Student of the Year Award in his pre-final year of bachelors.",
+    "Pratham has won Heritage Award for all-round excellence award back-to-back for 3 consecutive years.",
+    "Pratham has won Special Contribution to the Department Award for consecutive 2 years.",
+    "Pratham was an Undergraduate Teaching Assistant in his college, supporting freshers with a smooth transition to their college life.",
+    "The first book Pratham purchased for his interest was: Computer Networks: A top down approach.",
+    // "Out of interest, Pratham has read over 5 technical books and counting...",
+    "Pratham prefer Microservices over Monoliths.",
+    "Pratham prefers backend over frontend.",
+    "Pratham can build CICD pipelines from scratch.",
+    "Pratham can develop highly scalable, high throughput applications.",
+    "Pratham can build asynchronous servers.",
+    "Pratham can design and develop a production grade product, including microservices, libraries, testing pipelines, deployment, etc.",
+    "Pratham is not active on Social Media."
+];
 
 // FUNCTIONS
 /**
@@ -232,34 +445,61 @@ function takeCmdRelatedAction(cmd) {
 }
 
 function getResultText(cmd) {
-    // Add a switch case to return the result text based on the command
-    switch (cmd) {
-        case 'hi':
-            return "Hi! How are you doing";
-        case 'help':
-            return "Help executed";
-        case 'random':
-            return "Random executed";
-        default:
-            return "Invalid command";
-    }
+    let resultText = getRandomElement(GREET_KEYWORDS) + "! ";
+    var helpResultText = getRandomElement(HELP_CMD_PREFIX_LIST) + ": " + ALL_SUPPORTED_CMDS + ". " + CMDS_COMING_SOON;
 
+    switch (true) {
+        case SPECIAL_CMDS.includes(cmd):
+            resultText += 'This is a special command. Coming soon.....';
+            break;
+        case DEBUG_CMDS.includes(cmd):
+            resultText = "You have entered the debug section. Please type help and proceed ahead....";
+            break;
+        case cmd === 'help':
+            resultText += helpResultText;
+            break;
+        case BIO_CMDS.includes(cmd):
+            resultText += "Here's a quick info about Pratham. " + BIO_RESULT;
+            break;
+        case CONTACT_CMDS.includes(cmd):
+            resultText += CONTACT_RESULT;
+            break;
+        case RESUME_CMDS.includes(cmd):
+            resultText += RESUME_RESULT;
+            break;
+        case LINKEDIN_CMDS.includes(cmd):
+            resultText += LINKEDIN_RESULT;
+            break;
+        case GITHUB_CMDS.includes(cmd):
+            resultText += GITHUB_RESULT;
+            break;
+        case RANDOM_CMDS.includes(cmd):
+            resultText += "Here's a fact about Pratham. " + getRandomElement(RANDOM_RESULT_LIST);
+            break;
+        case dateCmds.includes(cmd):
+            resultText += "The current Date-Time Stamp is: " + getPrettyDateTime();
+            break;
+        case BLOCKED_CMDS.includes(cmd):
+            resultText = getRandomElement(FAILED_RESULT_PREFIX_ACTION_WORDS_LIST) + "! " + getRandomElement(BLOCKED_CMD_RESULT_LIST) + ".";
+            break;
+        default:
+            resultText = getRandomElement(FAILED_RESULT_PREFIX_ACTION_WORDS_LIST) + ", ";
+            resultText += getRandomElement(FAILED_RESULT_PREFIX);
+            resultText += `. ` + helpResultText;
+            break;
+    }
+    return resultText;
 }
 
-function getResultText(cmd) {
-    // Add a switch case to return the result text based on the command
-    switch (cmd) {
-        case 'hi':
-            return "Hi! How are you doing";
-        case 'help':
-            return "Help executed";
-        case 'random':
-            return "Random executed";
-        default:
-            return "Invalid command";
-    }
-
+/**
+ * Return a random element of the given list
+ * @param {object} listOfElement 
+ * @returns {object}
+ */
+function getRandomElement(listOfElement) {
+    return listOfElement[Math.floor(Math.random() * listOfElement.length)];
 }
+
 /**
  * A function that clears all the child nodes of a node.
  * @param {*} inputNode 
